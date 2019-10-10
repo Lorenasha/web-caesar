23420aa37903b6a45754e0ebe3145d81f685e179
Lorena Rodriguez <lorenasha_01@hotmail.com>
2019-10-10 14:08:29 -0400

    Adjusted main.py to show the encrypt text on the textarea of the original form

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ form="""
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
       <form action="/" method="POST">
-          <label for="rot">Rotate by:</label>
-        <input type="text" name="rot" id="rot" value="0" pattern="[0-9] {1-4}" required>
-        <textarea id="textarea" name="text" required> </textarea>    
+        <label for="rot">Rotate by:</label>
+        <input type="text" name="rot" id="rot" value="0" pattern="[0-9] required>
+        <textarea id="textarea" name="text" required > {0} </textarea>    
         <input type="submit" value="Submit Query">
       </form>
     </body>
@@ -36,14 +36,14 @@ form="""
 """
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/", methods=["POST"])
 def encrypt():
     rot=int(request.form["rot"])
     text=request.form["text"]
-
-    return "<h1>"+ rotate_string(text, rot )+"</h1>"
+    return form.format(rotate_string(text, rot ))
+    
     
     
 
